7d0b4ee288da9bcad2c1496c5bd2138083bb6311
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-03-09 16:00:21 +0500

    feat: create artist route

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TrackModule } from './track/track.module';
+import { ArtistModule } from './artist/artist.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { TrackModule } from './track/track.module';
     }),
     UserModule,
     TrackModule,
+    ArtistModule,
   ],
   controllers: [AppController],
   providers: [AppService],
